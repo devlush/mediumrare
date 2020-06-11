51956ec108db99288151fe8d0561593532dd06f5
Ian Gibbs <devlush@lemmaplex.org>
2020-06-11 18:56:39 -0500

    cosmetic: update comments

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,7 +3,7 @@ package main
 
 import "fmt"
 
-const N = 25
+const N = 25	// max limit for factors
 
 func build_terms() *[]string {
 
@@ -18,15 +18,23 @@ func build_terms() *[]string {
 
 func build_factors() *map[int][]int {
 
+	// map of integer arrays
 	fx := make(map[int][]int)
 
 	for i := 1; i <= N; i++ {
-		//fx[i] = append(fx[i], 1)
+	// loop over integers 1 to N
+	// calculate the possible factors for each integer
+	// and store as an integer array
 		for d := 2; d*d <= i; d++ {
+		// loop over integer divisors from
+		// zero up to the square root of i (product)
 			if i%d == 0 {
 				fx[i] = append(fx[i], d)
 			}
 		}
+		// for every factor found below the sqrt,
+		// divide it from the product (i) and store
+		// the quotient as an additional factor
 		for x := len(fx[i])-1; x >= 0; x-- {
 			hf := i / fx[i][x]
 			if hf * hf != i {
